e11c599d2fe0308f5da6c9d3d3f73ed70be6708b
Kirill Merkushev <lanwen@yandex.ru>
2017-01-09 15:27:40 +0300

    move owm token to separate file

--- weather-app/src/main/java/ru/yandex/autoschool/weather/WeatherApplication.java
+++ weather-app/src/main/java/ru/yandex/autoschool/weather/WeatherApplication.java
@@ -8,9 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import ru.yandex.autoschool.weather.clients.OpenWeatherClient;
 
 @SpringBootApplication
+@PropertySource(
+        ignoreResourceNotFound = true,
+        value = {
+                "classpath:owm.properties",
+                "file:/etc/weather/server/owm.properties"
+        }
+)
 public class WeatherApplication {
 
     public static void main(String[] args) {
